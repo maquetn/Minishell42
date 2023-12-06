a0a0ea06e986920caf050e0e40f7a5cb6f8334af
Noah Maquet <nmaquet@fu-r9-p4.s19.be>
2023-12-06 16:29:11 +0100

    exit

--- test.c
+++ test.c
@@ -2,28 +2,34 @@
 
 #define MAX_PATH_LENGTH 1024
 
-char *get_path() {
+char *get_path()
+{
     return getenv("PATH");
 }
 
-int try_executing(char *command, char *path) {
+int try_executing(char *command, char *path)
+{
     char full_path[MAX_PATH_LENGTH];
     char *path_ptr = path;
 
     pid_t pid = fork();  // Fork a new process
 
-    if (pid == -1) {
+    if (pid == -1)
+    {
         perror("fork");
         exit(EXIT_FAILURE);
     }
 
-    if (pid == 0) {
-        while (*path_ptr != '\0') {
+    if (pid == 0)
+    {
+        while (*path_ptr != '\0')
+        {
             // Find the next colon (:) in the path
             char *end = strchr(path_ptr, ':');
 
             // If no colon is found, set end to the end of the path string
-            if (end == NULL) {
+            if (end == NULL)
+            {
                 end = path_ptr + strlen(path_ptr);
             }
 
@@ -36,7 +42,8 @@ int try_executing(char *command, char *path) {
             strcat(full_path, command);
 
             // Try executing the command with the full path
-            if (access(full_path, X_OK) == 0) {
+            if (access(full_path, X_OK) == 0)
+            {
                 char *args[] = {command, NULL};
                 char *env[] = {NULL};  // Environment variables (empty for this example)
 
@@ -48,21 +55,32 @@ int try_executing(char *command, char *path) {
             }
 
             // Move to the next directory in the PATH
-            path_ptr = (*end == ':') ? end + 1 : end;
+            if (*end == ':')
+            {
+                path_ptr = end + 1;
+            }
+            else
+            {
+                path_ptr = end;
+            }
         }
 
         // If we reach this point, the command was not found in any of the directories
         fprintf(stderr, "Command not found: %s\n", command);
         exit(EXIT_FAILURE);  // Exit the child process if the command is not found
-    } else {
+    }
+    else
+    {
         // Parent process
         int status;
         waitpid(pid, &status, 0);  // Wait for the child process to complete
 
         // Check if the child process exited successfully
-        if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS) {
+        if (WIFEXITED(status) && WEXITSTATUS(status) == EXIT_SUCCESS)
+        {
             return 1;  // Return 1 to indicate the command was found and executed
-        } else {
+        } else
+        {
             return 0;  // Return 0 to indicate an error or that the command was not found
         }
     }
