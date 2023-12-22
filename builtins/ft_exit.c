85a0ff128c195d02d93efc64819689abb8c1b24a
Noah Maquet <nmaquet@fu-r10-p4.s19.be>
2023-12-22 16:21:28 +0100

    notes

--- builtins/ft_exit.c
+++ builtins/ft_exit.c
@@ -5,5 +5,12 @@ int ft_exit(int i, char **args)
 	g_exit_code = i;
 	if (args[1] && (atoi(args[1]) != 0))
 		g_exit_code = atoi(args[1]);
+	/*if (index > 1)
+		printf("exit: too many arguments");
+		//pas d exit
+	if ((atoi(args[1]) == 0))
+		printf("exit: test: numeric argument required");
+		//exit quand meme*/
+	printf("%d", g_exit_code);
 	exit(g_exit_code); //replace by exit code
 }
