b2f567d3077ef7744c8491bdf18b483a2a689fa7
Maxime Dor <mdor@shi-r6-p2.s19.be>
2024-01-09 12:28:21 +0100

    removed comments

--- exec.c
+++ exec.c
@@ -12,93 +12,6 @@
 
 #include "minishell.h"
 
-// int get_dollar(char *str, int i)
-// {
-//     while (str[i] != '$' && str[i] != '\0')
-//         i++;
-//     return (i);
-// }
-
-// char    *heredoc_dollar(char *str, t_minishell *data)
-// {
-//     char    *translated = NULL;
-//     char    *placeholder = NULL;
-//     char    *placeholder2 = NULL;
-//     int code;
-//     int i;
-
-//     i = 0;
-//     translated = ft_strdup("", data);
-//     code = 0;
-//     while (str[i] != '\0')
-//     {
-//         if (str[i] == '$' && str[i + 1] == '$')
-//         {
-//             code = (int)getpid();
-//             placeholder = ft_itoa(code, data);
-//             translated = ft_strjoin(translated, placeholder, data);
-//             i += 2;
-//         }
-//         else if (str[i] == '$' && str[i + 1] == '?')
-//         {
-//             code = data->exit_code;
-// 		    placeholder = ft_itoa(code, data);
-// 		    translated = ft_strjoin(translated, placeholder, data);
-//             i += 2;
-//         }
-//         else if (str[i] == '$' && (ft_isalnum(str[i + 1]) || str[i + 1] == '_'))
-//         {
-//             placeholder2 = ft_strndup(str, i + 1, get_cancer(str, i + 1) - 1, data);
-//             placeholder = get_env(placeholder2, data->env, data);
-//             if (placeholder == NULL)
-//             	placeholder = ft_strdup("", data);
-//             translated = ft_strjoin(translated, placeholder, data);
-//             i = get_cancer(str, i + 1);
-//         }
-//         else if (str[i] == '$')
-//         {
-//             translated = ft_strjoin(translated, "$", data);
-//             i++;
-//         }
-//         else
-//         {
-//             translated = ft_strjoin(translated, ft_strndup(str, i, get_dollar(str, i + 1) - 1, data), data);
-//             i = get_dollar(str, i);
-//         }
-//     }
-//     translated = ft_strjoin(translated, "\n", data);
-//     printf("trans :%s\n", translated);
-//     return (translated);
-// }
-
-// char    *heredoc_expander(char *str, char **file, t_minishell *data)
-// {
-//     char    *translated;
-
-//     translated = heredoc_dollar(str, data);
-//     *file = ft_strjoin(*file, translated, data);
-//     return (*file);
-// }
-
-// char    *manage_heredoc(char *delim, t_minishell *data)
-// {
-//     char    *file;
-//     char    *input;
-
-//     file = ft_strdup("", data);
-//     while (1)
-//     {
-//         input = readline("> ");
-//         if (ft_strcmp(delim, input) == 0)
-//             break;
-//         else
-//             file = heredoc_expander(input, &file, data);
-//         free(input);
-//     }
-//     free(input);
-//     return (file);
-// }
-
 void close_pipe(int pipe_fd[2]) 
 {
     close(pipe_fd[0]);
@@ -126,7 +39,7 @@ int open_all(t_simple_cmd *cmd)
     input_fd = 0;
     while(cmd->input)
     {
-		printf("input->name : %s\n", cmd->input->name);
+		//printf("input->name : %s\n", cmd->input->name);
         input_fd = open(cmd->input->name, O_RDONLY);
         if (cmd->input->next == NULL)
             break;
