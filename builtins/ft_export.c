85488a2d5f3caa4d440bace3602a26eafc381a28
Noah Maquet <nmaquet@shi-r9-p5.s19.be>
2024-01-12 18:29:34 +0100

    export +=test

--- builtins/ft_export.c
+++ builtins/ft_export.c
@@ -5,7 +5,7 @@ int syntax_env_var(char **args)
     int i = -1;
     while (args[1][++i] && args[1][i] != '=')
     {
-        if (isdigit(args[1][0]) || (!isalnum(args[1][i]) && args[1][i] != '_'))
+        if (isdigit(args[1][0]) || (!isalnum(args[1][i]) && args[1][i] != '_' && (args[1][i] == '+' && (i == 0 || args[1][i + 1] != '='))))
             return -1;
     }
     if (args[1][i] == '=')
@@ -16,6 +16,7 @@ int syntax_env_var(char **args)
 void ft_export(t_minishell *data, char **args)
 {
     int exp = 1;
+    //int plus_flag = 0;
     while (args[exp])
     {
         int env_count = 0;
@@ -34,8 +35,17 @@ void ft_export(t_minishell *data, char **args)
                 if (strncmp(data->env[env_count], args[1], syntax_env_var(args)) == 0 &&
                     data->env[env_count][syntax_env_var(args)] == '=')
                 {
-                    free(data->env[env_count]);
-                    data->env[env_count] = strdup(args[1]);
+                    if(data->env[env_count][syntax_env_var(args) - 1] == '+')
+                    {
+                        //APPEND
+                        printf("%s\n%s\n",data->env[env_count], args[1]);
+                        strncat(data->env[env_count], args[1] + syntax_env_var(args)+1, strlen(args[1]) - syntax_env_var(args));
+                    }
+                    else
+                    {
+                        free(data->env[env_count]);
+                        data->env[env_count] = strdup(args[1]);
+                    }
                     return;
                 }
                 env_count++;
@@ -51,6 +61,7 @@ void ft_export(t_minishell *data, char **args)
             }
 
             new_env[env_count] = strdup(args[1]);
+            printf("\n\nici\n\n");
             new_env[env_count + 1] = NULL;
 
             i = 0;
@@ -63,7 +74,6 @@ void ft_export(t_minishell *data, char **args)
 
             data->env = new_env;
         }
-        if (syntax_env_var(args) != -2)
 
         exp++;
     }
