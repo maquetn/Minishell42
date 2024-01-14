87c3ed1b28e68a0e192212c685d9c8a37dce8cc2
Maxime Dor <mdor@shi-r6-p2.s19.be>
2024-01-14 18:30:58 +0100

    norm

--- token.c
+++ token.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   token.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdor <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/01/14 18:20:07 by mdor              #+#    #+#             */
+/*   Updated: 2024/01/14 18:20:08 by mdor             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "minishell.h"
 
 void	free_tabl(char **tabl)
@@ -12,70 +24,74 @@ void	free_tabl(char **tabl)
 	}
 	free(tabl);
 }
-char    *ft_strndup(char *str, int start, int end, t_minishell *data)
-{
-    int len;
-    char    *dup;
-    int     i;
 
-    len = end - start + 1;
-    i = 0;
-    dup = gc_malloc(sizeof(char) * (len + 1), data);
+char	*ft_strndup(char *str, int start, int end, t_minishell *data)
+{
+	int		len;
+	char	*dup;
+	int		i;
+
+	len = end - start + 1;
+	i = 0;
+	dup = gc_malloc(sizeof(char) * (len + 1), data);
 	if (dup == NULL)
 		return (NULL);
-    while (i < len)
-    {
-        dup[i] = str[start + i];
-        i++;
-    }
-    dup[len] = '\0';
-    return (dup);
+	while (i < len)
+	{
+		dup[i] = str[start + i];
+		i++;
+	}
+	dup[len] = '\0';
+	return (dup);
 }
 
-
-void add_token(t_token **head, t_token_type type, char *content, t_minishell *data)
+void	add_token(t_token **head, t_token_type type, char *content, t_minishell *data)
 {
-	if (!content)
+	t_token	*new_token;
+	t_token	*current;
+
+	new_token = NULL;
+	current = NULL;
+	if (!content || content[0] == '\0')
 		return ;
-	if (content[0] == '\0')
+	new_token = gc_malloc(sizeof(t_token), data);
+	if (new_token == NULL)
 		return ;
-    t_token *new_token = gc_malloc(sizeof(t_token), data);
-    if (new_token == NULL)
-		return ;
-    new_token->type = type;
-    new_token->content = ft_strdup(content, data);
-    new_token->next = NULL;
-    new_token->prev = NULL;
+	new_token->type = type;
+	new_token->content = ft_strdup(content, data);
+	new_token->next = NULL;
+	new_token->prev = NULL;
 	new_token->quoted_heredoc = 0;
-    if (*head == NULL) 
-        *head = new_token;
+	if (*head == NULL) 
+		*head = new_token;
 	else 
 	{
-        t_token *current = *head;
-        while (current->next != NULL) 
+		current = *head;
+		while (current->next != NULL) 
 			current = current->next;
 		current->next = new_token;
-        new_token->prev = current;
-    }
+		new_token->prev = current;
+	}
 }
 
-void    print_tokens(t_token *head)
+void	print_tokens(t_token *head)
 {
 	printf("these are the tokens : \n\n");
-    while (head)
-    {
-        printf("type : %u content : %s\n", head->type, head->content);
-        head = head->next;
-    }
+	while (head)
+	{
+		printf("type : %u content : %s\n", head->type, head->content);
+		head = head->next;
+	}
 	printf("\nend of token display\n\n");
 }
 
-
-int get_next_token(char *str, int start)
+int	get_next_token(char *str, int start)
 {
-	while(str[start])
+	while (str[start])
 	{
-		if (str[start] == '>' || str[start] == '<' || str[start] == '|' || str[start] == ' ' || str[start] == '"' || str[start] == '\'' || str[start] == '\0')
+		if (str[start] == '>' || str[start] == '<' || str[start] == '|'
+			|| str[start] == ' ' || str[start] == '"'
+			|| str[start] == '\'' || str[start] == '\0')
 		{
 			return (start);
 		}
@@ -86,7 +102,7 @@ int get_next_token(char *str, int start)
 
 int	get_spc_chr(char *str, int start)
 {
-	while(str[start])
+	while (str[start])
 	{
 
 		if (str[start] == '>' || str[start] == '<' || str[start] == '|' || str[start] == ' ' || str[start] == '"' || str[start] == '\'')
@@ -100,7 +116,7 @@ int	get_spc_chr(char *str, int start)
 
 int	manage_double_quotes(char *str, int i, char **content, t_minishell *data)
 {
-	int start;
+	int	start;
 
 	start = i + 1;
 	while (str[i])
@@ -108,7 +124,8 @@ int	manage_double_quotes(char *str, int i, char **content, t_minishell *data)
 		i++;
 		if (str[i] == '\"' )
 		{
-			*content = ft_strjoin(*content, ft_strndup(str, start - 1, i, data), data);
+			*content = ft_strjoin(*content,
+					ft_strndup(str, start - 1, i, data), data);
 			return (i);
 		}
 	}
@@ -117,7 +134,7 @@ int	manage_double_quotes(char *str, int i, char **content, t_minishell *data)
 
 int	manage_single_quotes(char *str, int i, char **content, t_minishell *data)
 {
-	int start;
+	int	start;
 
 	start = i + 1;
 	while (str[i])
@@ -125,26 +142,14 @@ int	manage_single_quotes(char *str, int i, char **content, t_minishell *data)
 		i++;
 		if (str[i] == '\'' )
 		{
-			*content = ft_strjoin(*content, ft_strndup(str, start - 1, i, data), data);
+			*content = ft_strjoin(*content,
+					ft_strndup(str, start - 1, i, data), data);
 			return (i);
 		}
 	}
 	return (i);//si on arrive ici alors syntax error
 }
 
-int	something_behind(char *str, int i)
-{
-	i++;
-	while(str[i])
-	{
-		if (str[i] != ' ' && str[i] != '\0')
-			return (1);
-		i++;
-	}
-	printf("syntax error \n");
-	exit(EXIT_FAILURE);
-	return (0);
-}
 int	manage_output_append(char *str, int i, t_token **head, t_minishell *data)
 {
 	if (str[i + 1] == '>')
@@ -187,37 +192,40 @@ void	token(char *str, t_minishell *data)
 {
 	int		i;
 	char	*content;
-	t_token	*head = NULL; 
+	t_token	*head; 
 
 	i = 0;
+	head = NULL;
 	content = ft_strdup("", data);
 	while (str[i] != '\0')
 	{
 		if (str[i] == ' ')
 		{
 			if (i > 0)
-				if(str[i - 1] == '"' || str[i - 1] == '\'')
+			{
+				if (str[i - 1] == '"' || str[i - 1] == '\'')
 				{
 					add_token(&head, STR, content, data);
 					content = ft_strdup("", data);
 				}
-				i++;
-				continue;
+			}
+			i++;
+			continue ;
 		}
 		else if (str[i] == '>')
 		{
 			i = manage_output_append(str, i, &head, data) + 1;
-			continue;
+			continue ;
 		}
 		else if (str[i] == '<')
 		{
-			i = manage_input_heredoc(str, i, &head, data) +1;
-			continue;
+			i = manage_input_heredoc(str, i, &head, data) + 1;
+			continue ;
 		}
 		else if (str[i] == '|')
 		{
 			i = manage_pipe(i, &head, data) + 1;
-			continue;
+			continue ;
 		}
 		else if (str[i] == '\'')
 			i = manage_single_quotes (str, i, &content, data);
@@ -228,11 +236,12 @@ void	token(char *str, t_minishell *data)
 			i++;
 			if (str[i] == '\0')
 				add_token(&head, STR, content, data);
-			continue;
+			continue ;
 		}
 		else
 		{
-			content = ft_strjoin(content, ft_strndup(str, i, get_spc_chr(str, i), data), data);
+			content = ft_strjoin(content,
+					ft_strndup(str, i, get_spc_chr(str, i), data), data);
 			i = get_next_token(str, i);
 			if (str[i] != '\'' && str[i] != '"')
 			{
@@ -240,9 +249,9 @@ void	token(char *str, t_minishell *data)
 				content = ft_strdup("", data);
 			}
 			if (str[i + 1] == '\0' || data->error_trigger != 0)
-				break;
+				break ;
 		}
 	}
 	data->first_token = head;
-	//print_tokens(head);
 }
+	//print_tokens(head);
